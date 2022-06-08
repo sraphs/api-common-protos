54eeca55d774ca8e5b49b45e06dc5fcf848575ba
hydrz <n.haoyuan@gmail.com>
2022-06-08 14:33:23 +0800

    🐞fix: go_tags

--- regenerate.sh
+++ regenerate.sh
@@ -31,7 +31,7 @@ curl -sSL https://fastly.jsdelivr.net/gh/sraphs/slog@main/log.proto -o sraph/slo
 
 # go/tags
 mkdir -p go/tags
-curl -sSL https://fastly.jsdelivr.net/gh/sraphs/protobuf-go@reat/add_go_tag/gotags/opts.proto -o go/tags/opts.proto
+curl -sSL https://fastly.jsdelivr.net/gh/sraphs/protobuf-go@dev/gotags/opts.proto -o go/tags/opts.proto
 
 # copy and clean up
 rsync -av --include='*.proto' --include='*/' --exclude='*' ${tmpdir}/* ${dir}
